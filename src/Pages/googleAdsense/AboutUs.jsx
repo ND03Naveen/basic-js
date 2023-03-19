3b89609627a47406eeb6cffdcb3f9df7a68cd5c0
ND03Naveen <naveen@lambdazen.com>
2023-03-20 01:41:26 +0530

    about us and contact us update and logo update

--- src/Pages/googleAdsense/AboutUs.jsx
+++ src/Pages/googleAdsense/AboutUs.jsx
@@ -1,17 +1,21 @@
 import { Grid } from "@mui/material";
-function AboutUs (){
-    return(
+function AboutUs() {
+    return (
         <Grid>
-            <h2>About Basic Js</h2>
-            <p>It is created to help beginners to learn Javascript in a faster way by covering only the important topic and also it has exercises to practice the logic.</p>
-            <p>It is free to use and it has a comment feature on each page. so leaner can ask their doubts and clear them within 24 hours.</p>
-
-            <h2>Author</h2>
-            <p>Naveen. N.D</p>
-            <p>Web Developer</p>
-            <p>Email: ndnaveensweeebe@gmail.com</p>
-            <p>Mobile: +91 8124359871</p>
-
+            <h1 style={{borderBottom:"2px solid black",paddingBottom:"10px"}}>About Basic Js</h1>
+            <p style={{fontSize: "18px"}}>Welcome to Basic Js, an online platform designed to help beginners learn JavaScript in a faster and easier way. Our goal is to provide you with a comprehensive understanding of JavaScript fundamentals, while also allowing you to practice your skills through a series of exercises.</p>
+            <p style={{fontSize: "18px"}}>Our content covers only the most important topics of JavaScript, allowing you to learn quickly and efficiently. We also have a comments section on each page, where you can ask any questions you have and receive answers from our team of experts within 24 hours.</p>
+            <p style={{fontSize: "18px"}}>Our author, Naveen N.D, is a web developer with extensive experience in JavaScript and a passion for teaching others. He has carefully curated our content to ensure that it is easy to understand and highly practical.</p>
+            <p style={{fontSize: "18px"}}>In addition to our own content, we also recommend a range of resources to supplement your learning. These include:</p>
+            <ul style={{listStyleType: "square", fontSize: "18px",marginLeft: "30px"}}>
+                <li>"JavaScript: The Definitive Guide" by David Flanagan</li>
+                <li>"Eloquent JavaScript" by Marijn Haverbeke</li>
+                <li>"JavaScript and JQuery: Interactive Front-End Web Development" by Jon Duckett</li>
+                <li>"You Don't Know JS" by Kyle Simpson</li>
+                <li>"MDN Web Docs" - an online resource maintained by Mozilla</li>
+            </ul>
+            <p style={{fontSize: "18px"}}>We believe that with dedication and the right resources, anyone can learn JavaScript. So why not get started today? If you have any questions or feedback, please don't hesitate to get in touch with us at ndnaveensweeebe@gmail.com or call us at +91 8124359871.</p>
+            <p style={{fontSize: "18px"}}>Thank you for choosing Basic Js as your learning platform!</p>
         </Grid>
     )
 }
--- src/Pages/googleAdsense/ContactUs.jsx
+++ src/Pages/googleAdsense/ContactUs.jsx
@@ -1,24 +1,48 @@
 import { Grid } from "@mui/material";
+import "./ContactUs.css";
+
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 function ContactUs() {
     return (
         <Grid>
-            <Grid container style={{ justifyContent: "center" }}><h1>Send Your FeedBack</h1></Grid>
+            <h1>Contact Us</h1>
 
-            <Grid container style={{ justifyContent: "center" }}><h3>We will be happy to hear from you!</h3></Grid>
-
-            <div >
-                <div>
-                    <h3 style={{fontWeight:"bold"}}>How to contact us?</h3>
-                </div>
-                <p style={{fontSize:"medium",color:"#6C757D"}}>
-                    Feel free to send us an email anytime: <a href="mailto:ndnaveensweeebe@gmail.com">ndnaveensweeebe@gmail.com</a>
-                </p>
-                <p style={{fontSize:"medium",color:"#6C757D"}}>
-                    We will try our best to respond in <b>less than 24 Hours</b>, Thanks!
-
-                </p>
-
+            <div id="feedback">
+                <h2>Send Your Feedback</h2>
+                <p>We would love to hear from you! If you have any questions, comments, or suggestions, please don't hesitate to get in touch with us. You can reach us by email or social media.</p>
             </div>
+
+            <div id="contact-info">
+                <h2>How to Contact Us?</h2>
+                <p>Feel free to send us an email anytime:</p>
+                <a className="linka" href="mailto:ndnaveensweeebe@gmail.com">ndnaveensweeebe@gmail.com</a>
+            </div>
+            <div id="social-media">
+                <h2>Social Media</h2>
+                <ul className="customul">
+                    <li className="customli" >
+                        <a target="_blank" rel="noopener noreferrer" href="https://wa.me/8124359871"><WhatsAppIcon style={{ color: "#25D366" }}/></a>
+                    </li>
+                    <li className="customli" >
+                        <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/nd03nav"><InstagramIcon style={{ color: "#C13584" }}/></a>
+                    </li>
+                    <li className="customli" >
+                        <a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/100069198360952/"><FacebookIcon style={{ color: "#3B5998" }}/></a>
+                    </li>
+                    <li className="customli" >
+                        <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/nd03nav"><TwitterIcon style={{ color: "#1DA1F2" }}/></a>
+                    </li>
+                    <li className="customli" >
+                        <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/naveen-nd-7602a8158/"><LinkedInIcon style={{ color: "#0077B5" }}/></a>
+                    </li>
+                </ul>
+            </div>
+
+            <p>We will do our best to respond to your messages as quickly as possible, usually within 24 hours. Thanks for choosing us as your learning platform!</p>
         </Grid>
     )
 }
