33b1ed1fe79f509674385e01425a4754c3d7964f
ND03Naveen <naveen@lambdazen.com>
2023-10-07 22:23:42 +0530

    app update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-<<<<<<< HEAD
 import TempleDataupload from "./templeDataupload"
 // import ResponsiveDrawer from "./MUI Component/responsiveDrawer"
 
@@ -9,22 +8,15 @@ import TempleDataupload from "./templeDataupload"
 //   );
 // }
 import { Route, Routes } from "react-router-dom";
-=======
-import {ResponsiveDrawer} from "./MUI Component/responsiveDrawer"
->>>>>>> 714b651d98a809f523febd1a6de1a17815ec5a00
 
 function App() {
   let url  = window.location.href;
   return (
-<<<<<<< HEAD
     <React.Fragment>
             <Routes>
-                <Route path="/admin" element={<TempleDataupload/>}></Route>
+                <Route path="/" element={<TempleDataupload/>}></Route>
             </Routes>
         </React.Fragment>
-=======
-    <ResponsiveDrawer url={url}/>
->>>>>>> 714b651d98a809f523febd1a6de1a17815ec5a00
   );
 }
 
